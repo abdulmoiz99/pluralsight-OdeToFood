06a99a99475c400bdc82569a099e55da391c863c
moiz <moiza0799@gmail.com>
2020-08-15 13:19:43 +0500

    Adding Interface In OdeToFood.Data

---
+++ OdeToFood.Data/Services/IRestaurantData.cs
@@ -0,0 +1,22 @@
+﻿using OdeToFood.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Data.Services
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+
+        public class InMemoryRestaurantData : IRestaurantData
+        {
+            public IEnumerable<Restaurant> GetAll()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
