7624fdb310968703ab14fb8da56358b2d963b988
moiz <moiza0799@gmail.com>
2020-08-15 13:30:09 +0500

    Connecting InMemoryData Class With Interface

---
+++ OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,27 @@
+﻿using OdeToFood.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Data.Services
+{
+    class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant {Id = 1, Name = "Scott's Pizza" ,Cuisine =CuisineType.Italian},
+                new Restaurant {Id = 1, Name = "Tersiguels" ,Cuisine =CuisineType.French},
+                new Restaurant {Id = 1, Name = "Mango Grove " ,Cuisine =CuisineType.Pakistani},
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
