cc1776731b83d0e50f9ed47a209cfc5fdbc5aed6
moiz <moiza0799@gmail.com>
2020-08-19 20:24:17 +0500

    Understanding API and Web Controller and adding API controller

--- OdeToFood.Web/Global.asax.cs
+++ OdeToFood.Web/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
 
 namespace OdeToFood.Web
 {
@@ -15,6 +16,7 @@ namespace OdeToFood.Web
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             ContainerConfig.ResgisterContainer();
